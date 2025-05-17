106a8cde754852e7a578e9dcbd9750a02f9b29cc
Roughkingsir <saadkhan6224@yahoo.com>
2025-05-17 09:56:49 +0500

    testing through a different library

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/static/',  
+  base: '/static/',
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
-        '/api': 'http://localhost:8000',
+      '/api': 'http://localhost:8000',
     },
   },
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+    },
+  },
+});
