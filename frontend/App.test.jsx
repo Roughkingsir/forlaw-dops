08bb4d212c222a4ece3cce99a27f88e0e8a2f7dc
Roughkingsir <saadkhan6224@yahoo.com>
2025-05-20 16:18:23 +0500

    fixing imports

--- frontend/App.test.jsx
+++ frontend/App.test.jsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import App from './App';
+import App from './src/App';
 import { describe, it, expect } from 'vitest';
 
 describe('App component', () => {
